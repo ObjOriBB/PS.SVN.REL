560b7ee1a66db0224a5ae12401c9d5239613376e
urgotera <urgotera@gmail.com>
2017-09-01 20:36:41 +0800

    [170901.2036:Initial file for docs]

---
+++ PS.SVN.REL.mm
@@ -0,0 +1,91 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PS.SVN.REL" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1504269056281"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="Calibri" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Calibri" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Calibri" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Calibri" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="messagebox_warning"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="fork" TEXT_ALIGN="CENTER" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="14"/>
+<edge COLOR="#000000" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#000000" STYLE="fork" TEXT_ALIGN="LEFT" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="12"/>
+<edge COLOR="#009900" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" STYLE="fork" TEXT_ALIGN="LEFT" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="12"/>
+<edge COLOR="#00ff00" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" STYLE="fork" TEXT_ALIGN="LEFT" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="12"/>
+<edge COLOR="#99cc00" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" STYLE="fork" TEXT_ALIGN="LEFT" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="12"/>
+<edge COLOR="#cccc00" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" STYLE="fork" TEXT_ALIGN="LEFT" MAX_WIDTH="12.0 cm">
+<font NAME="Calibri" SIZE="12"/>
+<edge COLOR="#ffff00" WIDTH="1"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#000000"/>
+<node TEXT="REPOS" POSITION="right" ID="ID_1064961599" CREATED="1504269065229" MODIFIED="1504269074627">
+<edge COLOR="#00ff00"/>
+<node TEXT="SVN" ID="ID_1732661805" CREATED="1504269075747" MODIFIED="1504269078724">
+<node TEXT="src" ID="ID_454561222" CREATED="1504269082393" MODIFIED="1504269087592"/>
+<node TEXT="docs" ID="ID_389819341" CREATED="1504269087757" MODIFIED="1504269089445"/>
+<node TEXT="tdd" ID="ID_1100546979" CREATED="1504269090929" MODIFIED="1504269092479"/>
+</node>
+<node TEXT="GIT" ID="ID_1787944551" CREATED="1504269079285" MODIFIED="1504269080434">
+<node TEXT="master" ID="ID_104082243" CREATED="1504269226422" MODIFIED="1504269239890">
+<node TEXT="implicit src" ID="ID_776806478" CREATED="1504269292681" MODIFIED="1504269297557"/>
+</node>
+<node TEXT="docs" ID="ID_349962304" CREATED="1504269242323" MODIFIED="1504269243622">
+<node TEXT="not related to master" ID="ID_1469702735" CREATED="1504269307816" MODIFIED="1504269314505"/>
+<node TEXT="orphan" ID="ID_1439527963" CREATED="1504269320224" MODIFIED="1504269323034"/>
+</node>
+<node TEXT="tdd" ID="ID_549087863" CREATED="1504269246277" MODIFIED="1504269247064">
+<node TEXT="not related to master" ID="ID_800228726" CREATED="1504269324744" MODIFIED="1504269329639"/>
+<node TEXT="orphan" ID="ID_641356233" CREATED="1504269329983" MODIFIED="1504269331966"/>
+</node>
+</node>
+</node>
+</node>
+</map>
